c6ac24ec8d18795dbe7efb0d8b2286ef11bf842d
desssty <dimonmikryukov@gmail.com>
2025-05-17 01:06:36 +0400

    Подготовка spa

---
+++ frontend/src/pages/VisitorsPage/VisitorsPage.tsx
@@ -0,0 +1,12 @@
+import "./VisitorsPage.css";
+import type { FC } from "react";
+
+const VisitorsPage: FC = () => {
+  return (
+    <div className="visitors-page">
+      <h1>Список посетителей</h1>
+    </div>
+  );
+};
+
+export default VisitorsPage;
